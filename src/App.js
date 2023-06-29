0d1e77b28846c4bbfe816eaef2c82d2e5e4cf087
GabrielTrigueiro <gabrieltrigueirofernandes@gmail.com>
2023-06-29 09:07:10 -0300

    aplicando reset de css com sc GlobalStyle

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import React from "react";
+import React from 'react';
 
-import Cabecalho from "./Components/Cabecalho";
-import Container from "./Components/Container";
+import Cabecalho from './Components/Cabecalho';
+import Container from './Components/Container';
+import { GlobalStyle } from './Components/GlobalStyle';
 
 function App() {
   return (
     <>
+      {/* Importanto o global style aqui ele aplica o reset de css */}
+      <GlobalStyle />
+
       <Cabecalho />
       <Container />
     </>
